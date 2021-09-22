097fcac014f637b4558a5727a311090d274f9cbf
Rocío Garrido <rocioganon@gmail.com>
2021-09-22 18:55:38 +0200

    finish exercise 4 module 2

--- code/module-02/m02-start/module02.ts
+++ code/module-02/m02-start/module02.ts
@@ -72,13 +72,13 @@ function whichMonths(season: Season){
     return monthsInSeason;
 }
 
-console.log(whichMonths(Season.Fall);
+console.log(whichMonths(Season.Fall));
 
 /* EXERCISE 4
    TODO: Declare the array as the type to match the type of the items in the array. */
 
-   let randomNumbers;
-   let nextNumber;
+   let randomNumbers: number[] = [];
+   let nextNumber: number;
    
    for (let i = 0; i < 10; i++) {
        nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;

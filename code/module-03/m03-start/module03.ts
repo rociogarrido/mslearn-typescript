417bc808d13b3e127c18989871513b2962dd7778
Rocío Garrido <rocioganon@gmail.com>
2021-09-23 14:44:52 +0200

    finish ex 2 mod 3

--- code/module-03/m03-start/module03.ts
+++ code/module-03/m03-start/module03.ts
@@ -27,16 +27,16 @@ function calculateInterestOnlyLoanPayment(loanTerms: Loan): string {
 
 /*  TODO: Update the calculateConventionalLoanPayment function. */
 
-function calculateConventionalLoanPayment(principle, interestRate, months) {
+function calculateConventionalLoanPayment(loanTerms: ConventionalLoan): string {
     // Calculates the monthly payment of a conventional loan
-    let interest = interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
-    let payment;
-    payment = principle * interest / (1 - (Math.pow(1 / (1 + interest), months)));
+    let interest: number = loanTerms.interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
+    let payment: number;
+    payment = loanTerms.principal * interest / (1 - (Math.pow(1 / (1 + interest), loanTerms.months)));
     return 'The conventional loan payment is ' + payment.toFixed(2);
 }
 
-let interestOnlyPayment = calculateInterestOnlyLoanPayment(30000, 5);
-let conventionalPayment = calculateConventionalLoanPayment(30000, 5, 180);
+let interestOnlyPayment = calculateInterestOnlyLoanPayment({principal: 30000, interestRate: 5});;
+let conventionalPayment = calculateConventionalLoanPayment({principal: 30000, interestRate: 5, months: 180});
 
 console.log(interestOnlyPayment);     //* Returns "The interest only loan payment is 125.00" 
 console.log(conventionalPayment);     //* Returns "The conventional loan payment is 237.24" 

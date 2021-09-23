31274a112b8c6109aa8ea4109c331a21529c0b5c
Rocío Garrido <rocioganon@gmail.com>
2021-09-23 16:28:14 +0200

    finish ex 2 mod 4

--- code/module-04/m04-start/module04.ts
+++ code/module-04/m04-start/module04.ts
@@ -64,9 +64,12 @@ console.log(myArray2);
 /*  EXERCISE 2
     TODO: Update the LoanCalculator function. */
 
-function loanCalculator (principle, interestRate, months) {
-    let interest = interestRate / 1200;   // Calculates the monthly interest rate
-    let payment;
+function loanCalculator (principle: number, interestRate: number, months = 12): string {
+    let interest: number = interestRate / 1200;   // Calculates the monthly interest rate
+    let payment: number;
     payment = principle * interest / (1 - (Math.pow(1/(1 + interest), months)));
     return payment.toFixed(2);
 }
+
+let myLoan = loanCalculator(1000, 5);
+console.log(myLoan);

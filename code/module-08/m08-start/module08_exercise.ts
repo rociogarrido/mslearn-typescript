99042a4b62c29e98dd8bbcfccece9bd7ca8e2906
Rocío Garrido <rocioganon@gmail.com>
2021-09-24 17:15:32 +0200

    finish ex

---
+++ code/module-08/m08-start/module08_exercise.ts
@@ -0,0 +1,31 @@
+namespace Greetings {
+    export function returnGreeting (greeting: string) {
+        console.log(`The message from namespace Greetings is ${greeting}.`);
+    }
+}
+namespace GreetingsWithLength {
+    export function returnGreeting (greeting: string) {
+        let greetingLength = getLength(greeting);
+        console.log(`The message from namespace GreetingsWithLength is ${greeting}. It is ${greetingLength} characters long.`);
+    }
+    function getLength(message: string): number {
+        return message.length
+    }
+}
+
+namespace AllGreetings {
+    export namespace Greetings {
+        export function returnGreeting (greeting: string) {
+            console.log(`The message from namespace Greetings is ${greeting}.`);
+        }
+    }
+    export namespace GreetingsWithLength {
+        export function returnGreeting (greeting: string) {
+            let greetingLength = getLength(greeting);
+            console.log(`The message from namespace GreetingsWithLength is ${greeting}. It is ${greetingLength} characters long.`);
+        }
+        function getLength(message: string): number {
+            return message.length
+        }
+    }
+}
